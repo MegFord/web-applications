7e084d4f01dae799bdaee0133daa80d25e473a75
Meg Ford <meg387@gmail.com>
2014-05-28 22:20:58 -0500

    < ‘python/parsing notes’ > ‘python/Untitled Folder/parsing notes’ - ‘python/s.py’ + ‘python/scraper.py’ - ‘python/text’

---
+++ python/scraper.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+url_storage = []
+url = ''
+r  = requests.get(url)
+data = r.text
+soup = BeautifulSoup(data)
+with open('data.txt', 'w') as outfile:
+    for link in soup.find_all('a'):
+        l = link.get('href')
+        if l != None:
+            if l.startswith('/forums'):
+                url_storage.append(l)
+    json.dump(url_storage, outfile)  
