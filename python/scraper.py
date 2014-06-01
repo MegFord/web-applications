5e048089455ae6539792bb4022a7176c2771bb39
Meg Ford <meg387@gmail.com>
2014-06-01 13:57:38 -0500

    / ‘python/scraper.py’

--- python/scraper.py
+++ python/scraper.py
@@ -4,13 +4,38 @@ import requests
 
 url_storage = []
 url = ''
-r  = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data)
-with open('data.txt', 'w') as outfile:
+while url != '':
+    r  = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
     for link in soup.find_all('a'):
         l = link.get('href')
         if l != None:
             if l.startswith('/forums'):
-                url_storage.append(l)
+                l = junct_prepnd + l
+                url_storage[l] = url_storage.get(l, 'place_holder_text')
+            elif l.find('page') > -1:
+                page_link[l] = page_link.get(l, 0) + 1
+    for i, j in page_link.iteritems():
+        if j < 10:
+            page_link[i] = page_link.get(i) + 8
+            url = i
+            print url
+            break
+        else:
+            url = ''
+with open('data.txt', 'w') as outfile:
+    json.dump(url_storage, outfile)
+
+#oh why do you hang?
+for k, l in url_storage.iteritems():
+    if l == 'place_holder_text':
+        m = ''
+        r  = requests.get(k)
+        data = r.text
+        soup = BeautifulSoup(data)
+        for div in soup.find_all('div', class_='msgtxt'):
+            m += div.p.string
+            url_storage[k] = url_storage.set(k, m)
+with open('posts.txt', 'w') as outfile:
     json.dump(url_storage, outfile)  
