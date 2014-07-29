ec66135b23a15d8d506ff1e9d1bda8bbffc27ce5
Meg Ford <meg387@gmail.com>
2014-07-29 18:10:56 -0500

    + ‘python/Untitled Folder/ceylan.acl11.pdf’ + ‘python/Untitled Folder/document.pdf’ / ‘python/t.py’

--- python/t.py
+++ python/t.py
@@ -49,6 +49,7 @@ __email__ = "See the author's website"
 
 import collections
 from collections import defaultdict
+import csv
 import htmlentitydefs
 import json
 import operator
@@ -85,8 +86,11 @@ emoticon_string = r"""
     )"""
 username_string = r"""(?:@[\w_]+)"""
 hashtag_string = r"""(?:\#+[\w_]+[\w\'_\-]*[\w_]+)"""
-sentence_end =  r"""(?:[a-z]+[\.\?!]+[ ])""" 
+sentence_end =  r"""(?:[\.\?!])""" 
 url_string =  r"""(?:[htp]+[s]?[:/]+[a-z0-9]+[\w\d\.a-z/\?\=\&%\-]*)""" 
+apo_dash = r"""(?:[a-z][a-z'\-_]+[a-z])"""       
+elip_word = r"""(?:\.(?:\s*\.){1,})"""
+nums = r"""(?:[+\-]?\d+[,/.:-]\d+[+\-]?)""" 
 # The components of the tokenizer:
 regex_strings = (
     # Phone numbers:
@@ -124,13 +128,15 @@ regex_strings = (
     #url
     url_string
     ,
+    # Words with apostrophes or dashes.
+    apo_dash
+    ,
+    # Numbers, including fractions, decimals.
+    nums
+    ,
     # Remaining word types:
     r"""
-    (?:[a-z][a-z'\-_]+[a-z])       # Words with apostrophes or dashes.
-    |
-    (?:[+\-]?\d+[,/.:-]\d+[+\-]?)  # Numbers, including fractions, decimals.
-    |
-    (?:[\w_]+)                     # Words without apostrophes or dashes.
+    (?:[\w_]+)                     
     |
     (?:\.(?:\s*\.){1,})            # Ellipsis dots. 
     #| 
@@ -149,6 +155,8 @@ username_re = re.compile(regex_strings[3], re.VERBOSE | re.I | re.UNICODE)
 hashtag_re = re.compile(regex_strings[4], re.VERBOSE | re.I | re.UNICODE)
 sentence_end_re = re.compile(regex_strings[5], re.VERBOSE | re.I | re.UNICODE)
 url_re = re.compile(regex_strings[6], re.VERBOSE | re.I | re.UNICODE)
+punct_re = re.compile(regex_strings[7], re.VERBOSE | re.I | re.UNICODE)
+num_re = re.compile(regex_strings[9], re.VERBOSE | re.I | re.UNICODE)
 
 # These are for regularizing HTML entities to Unicode:
 html_entity_digit_re = re.compile(r"&#\d+;")
@@ -236,9 +244,10 @@ class Tokenizer:
             word = "username " #+ word
         elif hashtag_re.search(word):
             word = "hashtag " #+ word
-        elif sentence_end_re.search(word):
-            word = word
+            
+        word = re.sub(",", " ", word)
         return word.lower()
+
 
 class NGram_Helpers:   
     
@@ -247,28 +256,52 @@ class NGram_Helpers:
     forum_three_dict = {}
     forum_two_dict = {}
     forum_one_dict = {}
-    METHOD_NAME = { 'PARSE':0, 'TOKENIZE':1, 'SPLIT':2, 'COUNT':3, 'HASH':4 }
+    METHOD_NAME = { 'PARSE':0, 'TOKENIZE':1, 'SPLIT':2, 'COUNT':3, 'HASH':4, 'REPLACE':5 }
         
         
     def __init__(self,samples):
-        self.unigrams = self.loop(samples,1)
-        self.bigrams = self.loop(samples,2)
-        self.trigrams = self.loop(samples,3)
+        self.unigrams = self.loop(samples,1,1)
+        self.bigrams = self.loop(samples,2,1)
+        self.trigrams = self.loop(samples,3,1)
         self.total_words = sum(self.unigrams.values())
-   
+        
+    """
+    loop tokenizes tweets before building ngrams
+    """
     #refactor along with build_forum     
-    def loop(self, samples, num):
+    def loop(self, samples, num, method_name):
         n_list = []
         n_dict = {}    
         for s in samples:
-            n_list = self.build_tweet(s, num, 0)
+            n_list = self.build_tweet(s, num, method_name)
             n_list = self.build_ngrams(n_list, num)
             self.hash_dict.update(self.count_gram(n_list, self.hash_dict))
             n_dict.update(self.count_gram(n_list, n_dict))
         return  n_dict
 
+    def build_forum(self, samples, num, method_name):
+        self.clear_dicts()
+        parsed = []
+        if method_name == self.METHOD_NAME.get('COUNT'):
+            parsed = self.build_tweet(samples, num, 2)
+            return parsed
+        else:
+            parsed = self.build_tweet(samples, num, 5)
+            self.forum_three_dict.update(self.count_gram(self.build_ngrams(parsed, num), self.forum_three_dict))
+            self.forum_two_dict.update(self.count_gram(self.build_ngrams(parsed, num-1), self.forum_two_dict))
+            self.forum_one_dict.update(self.count_gram(self.build_ngrams(parsed, num-2), self.forum_one_dict))  
+    
+    """
+    Method to return individual sentences, sorted by highest probability
+    """        
+    def test_data(self, samples, num, method_name):
+        self.clear_dicts()
+        parsed_sentence = []
+        for s in samples:
+            parsed_sentence += self.build_tweet(samples, num, 4)
+            
     def build_tweet(self, s, num, method_name):
-	tokenized = []
+        tokenized = []
         for i in range(0, num - 1):
             tokenized += ["*"]
         if method_name == self.METHOD_NAME.get('TOKENIZE'):
@@ -279,18 +312,7 @@ class NGram_Helpers:
             for single_line in s:
             	tokenized += single_line.split()
         tokenized += ["~STOP~"]  
-        return tokenized
-
-    def build_forum(self, samples, num, method_name):
-        self.clear_dicts()
-        parsed = []
-        parsed = self.build_tweet(samples, num, 2)
-        if method_name == self.METHOD_NAME.get('COUNT'):
-            return parsed
-        else:
-            self.forum_three_dict.update(self.count_gram(self.build_ngrams(parsed, num), self.forum_three_dict))
-            self.forum_two_dict.update(self.count_gram(self.build_ngrams(parsed, num-1), self.forum_two_dict))
-            self.forum_one_dict.update(self.count_gram(self.build_ngrams(parsed, num-2), self.forum_one_dict))            
+        return tokenized          
     
     def clear_dicts(self):
         self.forum_three_dict.clear()
@@ -300,7 +322,6 @@ class NGram_Helpers:
     def build_ngrams(self, tokenized, num):
         hash_list = []   
         for i in range(len(tokenized)-(num-1)):
-            print tokenized[i]
     	    hash_gram = "_".join(tokenized[i:i+num])
             hash_list.append(hash_gram)
             #print hash_list.count(hash_gram)
@@ -309,6 +330,8 @@ class NGram_Helpers:
     def count_gram(self, ngram_list, hash_gram):
         for gram in ngram_list:
             #print gram
+            #re-write to use 1gram (first word) as key, store 3grams + counts as values, so 2grams can be re-constructed?
+            #what are the trade-offs of storage vs. retrieval costs?
             hash_gram[gram] = hash_gram.get(gram, 0) + 1
             #print hash_gram.get(gram)
         return hash_gram
@@ -325,22 +348,28 @@ class NGram_Helpers:
               count_list.append(0.0) 
         return special_case, count_list
 
-    def probability(self, count_3gram, count_2gram,count_1gram, l1, l2, l3, l4):
-        feq = [self.get_ratio(x, y, z, l1, l2, l3, l4) for x, y, z in zip(count_3gram, count_2gram, count_1gram)]
-        return reduce(operator.imul, feq)
+    def probability(self, count_3gram, count_2gram,count_1gram, l1, l2, l3, l4, total_words):
+        feq = [self.get_ratio(x, y, z, l1, l2, l3, l4, total_words) for x, y, z in zip(count_3gram, count_2gram, count_1gram)]
+        if len(feq) > 0:
+            return reduce(operator.imul, feq)
+        else:
+            return l4 * 1/(2 * total_words)
         
-    def start_probability(self, count_3gram, count_2gram, l1, l4):
-        feq = [self.get_start_ratio(x, y, l1, l4) for x, y in zip(count_3gram, count_2gram)]
-        return reduce(operator.imul, feq)
+    def start_probability(self, count_3gram, count_2gram, l1, l4, total_words):
+        feq = [self.get_start_ratio(x, y, l1, l4, total_words) for x, y in zip(count_3gram[:len(count_2gram)], count_2gram)]
+        if len(feq) > 0:
+            return reduce(operator.imul, feq)
+        else:
+            return l4 * 1/(2 * total_words)
         
-    def get_start_ratio(self, x, y, l1, l4):
+    def get_start_ratio(self, x, y, l1, l4, total_words):
         p_3gram = 0        
         if(y != 0):
             p_3gram = x/y
-        return (l1 * p_3gram) + (l4 * 1/(2 * self.total_words))     
+        return (l1 * p_3gram) + (l4 * 1/(2 * total_words))     
 
     # smoothing so we don't end up with div by zero
-    def get_ratio(self, x, y, z, l1, l2, l3, l4):
+    def get_ratio(self, x, y, z, l1, l2, l3, l4, total_words):
         p_3gram = 0
         p_2gram = 0
         p_1gram = 0
@@ -348,12 +377,12 @@ class NGram_Helpers:
             p_3gram = x/y
         if(z != 0):
             p_2gram = y/z       
-            p_1gram = z/self.total_words
-        return (l1 * p_3gram) + (l2 * p_2gram) + (l3 * p_1gram) + (l4 * 1/(2 * self.total_words))       
+            p_1gram = z/total_words
+        return (l1 * p_3gram) + (l2 * p_2gram) + (l3 * p_1gram) + (l4 * 1/(2 * total_words))       
             
 class File_Utils:
     
-    def crawl_directory(self,root_dir="~/Tweets"):
+    def crawl_directory(self, root_dir="~/Tweets"):
         file_group = []
         tweet_path = os.path.expanduser(root_dir)
         file_group = [f for f in os.listdir(tweet_path) if os.path.isfile(os.path.join(tweet_path, f))] 
@@ -364,9 +393,52 @@ class File_Utils:
         tweet_path = os.path.expanduser(root_dir)
         for tweet_file in file_group:
             samples.extend(open(os.path.join(tweet_path, tweet_file)))
-        #for t in samples:
-         #   print t
         return samples
+        
+    def gather(self, samples, root_dir="~/Tweets"):
+        tweet_path = os.path.expanduser(root_dir)
+        for tweet_file in samples:
+            samples = []
+            t = ""
+            samples.extend(open(os.path.join(tweet_path, tweet_file)))
+            for s in samples:
+                t += self.clean_posts(s)
+            with open(os.path.join(tweet_path, tweet_file), 'w') as outfile:
+                outfile.write(t)
+                
+    def clean_posts(self, word):
+        word = re.sub(r'99 for 2 day shipping if you dont', "", word)
+        word = re.sub(r'Debbie Riddle, R-Tomball, the House sponsor of the measure', "", word)
+        word = re.sub(r'Prize will be shipped to the winner at no cost to the winner', "", word)
+        word = re.sub(r'Happy Birthday To You', "", word)
+        word = re.sub(r'For complete rules, please visitBellaOnline ALERT:  Raw URLs are not allowed in these forums for security reasons', "", word)
+        word = re.sub(r'FarjanaEnvironment at BellaOnline', "", word)
+        word = re.sub(r'Julie BaumlerComputer Careers EditorComputer Careers ForumJulie', "", word) 
+        word = re.sub(r'com', "", word)
+        word = re.sub(r'49 (free ship because I have Amazon Prime), but + 2', "", word)
+        word = re.sub(r'Stephanie WatsonWeight Loss EditorI agree with Lynn', "", word)
+        word = re.sub(r'ResultsNo HomeworkWeak Memory', "", word)
+        word = re.sub(r'comSince November of 2004', "", word)
+        word = re.sub(r'\*HINT\*BellaOnline ALERT:  Raw URLs are not allowed in these forums for security reasons', "", word)
+        word = re.sub(r'SueEarly Childhoodhttp://www', "", word)
+        word = re.sub(r'ElleiseClairvoyance Editor', "", word)
+        word = re.sub(r'\(adsbygoogle = window', "", word)
+        
+        word = re.sub(r'_________________________', "", word)
+        word = re.sub(r'Hope Fields\"Though I am one person,the voice which I use, to speak out against domestic violence, is the voice of many.\"', "", word)
+        word = re.sub(r'JanetJanetBoyer.com', "", word)
+        word = re.sub(r'Jeanette Stingley - Women\'s Lithttp://womenslit.bellaonline', "", word)
+        return word
+        
+    def remove_punct(self, samples):
+        temp = []
+        for s in samples:
+            if s != '\n':
+                if sentence_end_re.search(s):
+                    temp += re.split(sentence_end_re, s)
+                else:
+                    temp += [s]            
+        return temp
         
     def write_json(self, term_doc_matrix, file_name, root_dir='~/forumPost'):
         tweet_path = os.path.expanduser(root_dir)
@@ -380,6 +452,15 @@ class File_Utils:
             d = json.load(f)
             d = json.JSONDecoder().decode(d)
         return d
+        
+    def write_prob(self, sentence, file_name, root_dir='~/forumPost'):
+        write_path = os.path.expanduser(root_dir)
+        writer = csv.writer(open(os.path.join(write_path, file_name), 'wb'))
+        #sort ordered dict highest to lowest probability
+        items = sentence.items()
+        items.reverse()
+        for key, value in sentence.items():
+            writer.writerow([key, value])
 
 ###############################################################################
 
@@ -389,62 +470,90 @@ if __name__ == '__main__':
 
     samples = []
     posts = []
-    file_group = fi.crawl_directory()
-    samples += fi.create_samples(file_group)
+    temp = []
+    prob = collections.OrderedDict()
+    L1 = 0.85
+    L2 = 0.1
+    L3 = 0.04
+    L4 = 0.01
+    st_pr = 0.0
+    training_dir = "~/forums"
+    out_path = '~/forumPost'
+    test_path = '~/testData'
+    """
+    #clean data  
+    file_group = fi.crawl_directory("~/forum1")
+    fi.gather(file_group,"~/forum1")
+    file_group2 = fi.crawl_directory("~/forum2")
+    fi.gather(file_group2,"~/forum2")
+    forum_samples = fi.crawl_directory(test_path)
+    fi.gather(forum_samples, test_path)
+
+    #Section to create training data
+    training_dir = "~/forums"
+    file_group = fi.crawl_directory("~/forum1")
+    file_group2 = fi.crawl_directory("~/forum2")
+   
+    samples += fi.create_samples(file_group,"~/forum1")
+    samples += fi.create_samples(file_group2,"~/forum2") 
+    
+    samples = fi.remove_punct(samples)
+    #print samples
     n = NGram_Helpers(samples)
-    #training data
-    n.build_forum(samples, 3, 4)
-    fi.write_json(n.forum_three_dict, "threeGram.json")
-    fi.write_json(n.forum_two_dict, "twoGram.json")
-    fi.write_json(n.forum_one_dict, "oneGram.json")
-    #test data
-    forum_samples = fi.crawl_directory('~/forums')
-    posts += fi.create_samples(file_group)
-    ngram_base = n.build_tweet(posts, 3, 2)
-    tri_gram = n.build_ngrams(ngram_base, 3)
-    duo_gram = n.build_ngrams(ngram_base, 2)
-    uno_gram = n.build_ngrams(ngram_base, 1)
+    fi.write_json(n.trigrams, "threeGram.json")
+    fi.write_json(n.bigrams, "twoGram.json")
+    fi.write_json(n.unigrams, "oneGram.json")
+    #end section to create training data
+    """
+    lineThreeGram = fi.read_json("threeGram.json")
+    lineTwoGram = fi.read_json("twoGram.json")
+    lineOneGram = fi.read_json("oneGram.json") 
+
+    forum_samples = fi.crawl_directory(test_path)
+    posts += fi.create_samples(forum_samples, test_path)
+    #print posts
+    forum_samples = fi.remove_punct(posts)
+    n = NGram_Helpers(forum_samples)
+    for f in forum_samples:
+        ngram_base = n.build_tweet(f, 3, 1)
+        tri_gram = n.build_ngrams(ngram_base, 3)
+        duo_gram = n.build_ngrams(ngram_base, 2)
+        uno_gram = n.build_ngrams(ngram_base, 1)
+        length = sum(lineOneGram.values())
+
+        count_3_list = n.pr_gram(lineThreeGram, tri_gram)
+        count_2_list = n.pr_gram(lineTwoGram, duo_gram[:len(tri_gram)])
+        count_1_list = n.pr_gram(lineOneGram, uno_gram[:len(tri_gram)])
+        if len(count_3_list[0]) > 0:
+            st_pr = n.start_probability(count_3_list[0], count_2_list[0], L1, L4, length)
+        if len(count_3_list[1]) > 0:
+            pr = n.probability(count_3_list[1], count_2_list[1], count_1_list[1], L1, L2, L3, L4, length)
+        if st_pr != 0.0 and pr != 0.0:
+            pr = st_pr * pr
+        prob[f] = pr
+    fi.write_prob(prob,"probability.csv")
+   
+    """
     fi.write_json(tri_gram, "forumThreeGram.json")
     fi.write_json(duo_gram, "forumTwoGram.json")
     fi.write_json(uno_gram, "forumOneGram.json")
+    """
     #now test the test
-    out_path = '~/forumPost'
+    """
     lineThreeGram = fi.read_json("threeGram.json")
     lineOneGram = fi.read_json("oneGram.json") 
     lineTwoGram = fi.read_json("twoGram.json") 
     inputTwoGram = fi.read_json("forumTwoGram.json")
     inputOneGram = fi.read_json("forumOneGram.json")  
     inputThreeGram = fi.read_json("forumThreeGram.json") 
-    length = len(lineThreeGram) 
-    count_3_list = n.pr_gram(lineThreeGram, inputThreeGram)
-    print inputThreeGram
-    print len(inputThreeGram)
-    print inputTwoGram[:len(inputThreeGram)]
-    print len(inputTwoGram[:len(inputThreeGram)])
-    print inputOneGram[:len(inputThreeGram)] 
-    count_2_list = n.pr_gram(lineTwoGram, inputTwoGram[:len(inputThreeGram)])
-    len(count_2_list)
-    count_1_list = n.pr_gram(lineOneGram, inputOneGram[:len(inputThreeGram)])
-    L1 = 0.85
-    L2 = 0.1
-    L3 = 0.04
-    L4 = 0.01
-    st_pr = n.start_probability(count_3_list[0], count_2_list[0], L1, L4)
-    print count_1_list[1]
-    print count_2_list[1]
-    print count_3_list[1]
-    pr = n.probability(count_3_list[1], count_2_list[1], count_1_list[1], L1, L2, L3, L4)
-    print pr
-    if st_pr != 0 and pr != 0:
-        pr = st_pr * pr
-        print st_pr
-    print pr
+    """
     
     #ask for user input
     
     #train with 90% of forum data test with 10% of forum data what is the average probability of a sentence
-   # output 50 probabilities and save to excel
+    # output 50 probabilities and save to excel
     
     #test with tweets and find average
-     # output 50 probabilities and save to excel
+    # output 50 probabilities and save to excel
+
     
