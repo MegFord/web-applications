64e952d6621d61d05eb00b2bd2259d39cc8002c5
Meg Ford <meg387@gmail.com>
2014-08-03 10:49:35 -0500

    / ‘python/t.py’

--- python/t.py
+++ python/t.py
@@ -489,7 +489,7 @@ class File_Utils:
         return ret
         
     def tweet_pr(self, tweet):
-        tweet = tweet.decode("utf-8")
+        tweet = tweet.encode("utf-8")
         pr = 0
         st_pr = 0
         ngram_base = NGram_Helpers.build_tweet(tweet, 3, 1)
